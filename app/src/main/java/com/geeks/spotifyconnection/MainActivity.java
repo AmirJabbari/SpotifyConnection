595643b131e30389048bbbb8d60226dbb55adbd7
AmirJabbari <am.jabbarii@gmail.com>
2021-06-10 00:59:51 +0430

    Add Spotify-app-remote file Authentication and Connect to Spotify

--- app/src/main/java/com/geeks/spotifyconnection/MainActivity.java
+++ app/src/main/java/com/geeks/spotifyconnection/MainActivity.java
@@ -1,14 +1,69 @@
 package com.geeks.spotifyconnection;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String REDIRECT_URI = "my-awesome-app-login://callback";
+    private static final String CLIENT_ID = "4828bb77a76a498bb9268b7d71cc296c";
+    private static final int REQUEST_CODE = 1337;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Check if result comes from the correct activity
+        Log.e("MainActivity", "onActivityResult");
+
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+
+
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    Log.e("MainActivity", "Error response");
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    Log.e("MainActivity", "");
+
+                    // Handle other cases
+            }
+        }
+    }
+
 }
