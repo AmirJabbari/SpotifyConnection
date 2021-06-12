3fb849ff21c3ce7f5ef6e53dbc1fb0161c30e37a
AmirJabbari <am.jabbarii@gmail.com>
2021-06-12 23:56:51 +0430

    Added SharedPreferences

---
+++ app/src/main/java/com/geeks/spotifyconnection/config/PrefManager.java
@@ -0,0 +1,21 @@
+package com.geeks.spotifyconnection.config;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "setting";
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+}
